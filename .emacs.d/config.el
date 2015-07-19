215c6cfc8e919abf99f8222a3cca1ea9df05a470
Timm <tymmm1@gmail.com>
2015-07-19 15:33:39 +0200

    Added space between line numbers and code.

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -5,6 +5,7 @@
 (ac-config-default)
 ;; Show line numbers
 (global-linum-mode t)
+(setq linum-format "%4d  ")
 ;; Set up python auto completion
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
