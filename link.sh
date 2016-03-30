ad21a60053f716e83d70e9d6a0052d93ca46e10e
Timm <tymmm1@gmail.com>
2016-03-29 19:12:30 -0700

    Link dotfiles to symbolic links in the home directory

---
+++ link.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~
+ln -s .dotfiles/.emacs.d .emacs.d
+ln -s .dotfiles/.oh-my-zsh .oh-my-zsh
+ln -s .dotfiles/.vim .vim
+ln -s .dotfiles/.Xmodmap .Xmodmap
+ln -s .dotfiles/.Xresources .Xresources
+ln -s .dotfiles/.aliases .aliases
+ln -s .dotfiles/.dir_colors .dir_colors
+ln -s .dotfiles/.tmux.conf .tmux.conf
+ln -s .dotfiles/.vimrc .vimrc
+ln -s .dotfiles/.xsession .xsession
+ln -s .dotfiles/.zshrc .zshrc
+mkdir -p .config/i3 && ln -s ~/.dotfiles/i3_config ~/.config/i3/config
+
